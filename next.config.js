7e78eb14ea1d17c41ad6e7eda82b4b2ca8223150
Jaeseob Hwang <jetsbee@gmail.com>
2023-11-03 12:17:15 +0900

    배포이후 Next.js 서버가 필요없는 SPA 프로젝트로 변경

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export", // ref. https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
