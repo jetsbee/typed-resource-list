865042215455bf41d2448727b76a4c4ae5f5dee9
Jaeseob Hwang <jetsbee@gmail.com>
2023-11-05 19:37:16 +0900

    Remove mock files

--- src/utils/mocks/getBaboonImgFile.ts
+++
@@ -1,5 +0,0 @@
-export const getBaboonImgFile = async () => {
-  const baboonBlob = await (await fetch("/4.2.03.png")).blob();
-
-  return new File([baboonBlob], "4.2.03.png", { type: baboonBlob.type });
-};
